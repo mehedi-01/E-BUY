9b61d245da7d71a32d0a2e6f801c3ff4d0e99b64
Md Mehedi Hasan <45948603+mehedi-01@users.noreply.github.com>
2021-11-15 00:22:55 +0600

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 /*===== MENU SHOW =====*/ 
-
-
+const showMenu =(toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle(show)
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
 /*===== REMOVE MENU MOBILE =====*/
